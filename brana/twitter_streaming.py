57e42ec0affadab91a30905a7be8b2f1d5bffc66
Toshal Patel <toshaltpatel@gmail.com>
2017-10-07 20:48:26 +0530

    Add files via upload

--- brana/twitter_streaming.py
+++ brana/twitter_streaming.py
@@ -19,23 +19,27 @@ oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 twitter_stream = TwitterStream(auth=oauth)
 
 # Get a sample of the public data following through Twitter
-iterator = twitter_stream.statuses.filter(track="Google", language="en")
+iterator = twitter_stream.statuses.filter(track="Patanjali", language="en")
+
+# Create a file to write all the tweets
+fhandle = open("tweets_patanjali.txt", "w")
 
 # Print each tweet in the stream to the screen 
 # Here we set it to stop after getting 1000 tweets. 
 # You don't have to set it to stop, but can continue running 
 # the Twitter API to collect data for days or even longer.
-
-tweet_count = 50
+tweet_count = 1000
 for tweet in iterator:
     tweet_count -= 1
     # Twitter Python Tool wraps the data returned by Twitter 
     # as a TwitterDictResponse object.
     # We convert it back to the JSON format to print/score
-    print json.dumps(tweet)  
+    fhandle.write(json.dumps(tweet))
     
     # The command below will do pretty printing for JSON data, try it out
-    # print(json.dumps(tweet, indent=4))
+    # print json.dumps(tweet, indent=4)
        
     if tweet_count <= 0:
-        break 
+        break
+
+fhandle.close()
